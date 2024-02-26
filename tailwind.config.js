6152fa01399733ed07d96dd2bff113983c7487fd
Gerardo Saz <gerardo.saz120@gmail.com>
2024-02-25 18:44:00 -0600

    dark mode + colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius:{
+        DEFAULT: "var(--rounded)" 
+      },
+      backgroundImage:{
+        "gradient-principal": "linear-gradient(var(--gradient-principal))",
+        "aqua-gradient": "linear-gradient(var(--aqua-gradient))",
+        "blue-gradient": "linear-gradient(var(--blue-gradient))",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        "action-bg-button": "var(--action-bg-button)",
+        "action-text-button": "var(--action-text-button)",
+        "text-secundary": "var(--text-secundary)",
+        "yellow-bg-notification": "var(--yellow-bg-notification)",
+        "yellow-border-notification": "var(--yellow-border-notification)",
+        "yellow-text-notification": "var(--yellow-text-notification)",
+        "light-blue": "var(--light-blue)",
+
+      }
+    },
   },
   plugins: [],
 }
