50935780d40cd97c8715c71fa5e1c9ec99a88852
Joaco <fuentesmo.jaf@gmail.com>
2024-02-28 14:24:17 -0600

    seeeeeeee

--- src/pages/UserPages/Login.jsx
+++ src/pages/UserPages/Login.jsx
@@ -5,16 +5,32 @@ const Login = () => {
     return (
         <>
             <FormLayout>
-                <h1 className='font-bold text-2xl'>Iniciar Sesion</h1>
-                <p>Accede a nuestra plataforma</p>
-                <label>email</label>
-                <input placeholder='email' />
-                <label>Contraseña</label>
-                <input placeholder='Contraseña' />
-                <p>Olvidaste tu contraseña?</p>
-                <button>Iniciar Sesion</button>
+                <div className='flex flex-col gap-4'>
+                    <h1 className='font-bold text-2xl text-center'>Iniciar Sesion</h1>
+                    <p className='text-text-secundary text-center'>Accede a nuestra plataforma</p>
+                    <label className='font-normal'>Email</label>
+                    <Input 
+                    placeholder={"Email"}
+                    name={"email"}
+                    type={"email"}
+                    />
+                    <label className='font-normal'>Contraseña</label>
+                    <Input 
+                    placeholder={"Contraseña"}
+                    name={"password"}
+                    type={"password"}
+                    />
+                    <p className='text-text-secundary text-sm underline'>Olvidaste tu contraseña?</p>
+                    <button className='p-3 bg-action-bg-button text-action-text-button rounded-md font-bold'>Iniciar Sesion</button>
+                </div>
             </FormLayout>
         </>
+    )
+}
+
+function Input({type,name,placeholder}){
+    return(
+        <input className='border border-border rounded-md p-2' type={type} name={name} placeholder={placeholder}></input>
     )
 }
 
